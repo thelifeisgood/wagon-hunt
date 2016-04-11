ea78d4531a82850884df106b7e57d2ea618f4b4f
thelifeisgood <besson.antho@hotmail.fr>
2016-04-11 18:50:37 +0200

    add tagline to product table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411154604) do
+ActiveRecord::Schema.define(version: 20160411164907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160411154604) do
     t.string   "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "tagline"
   end
 
 end
