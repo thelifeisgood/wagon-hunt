66b3b5d346420f30da4f99bb1717cde5457a2fd7
thelifeisgood <besson.antho@hotmail.fr>
2016-04-11 18:47:13 +0200

    add model product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
+  	@today = Date.today
+  end
+
+  def team
+  	@members = ["Antho", "Yoann"]
+  end
+
+  def join_us
+  	render plain: "Send us a mail"
   end
 end
