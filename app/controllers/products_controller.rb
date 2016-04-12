d798f1f63eaa5405595b1d6f8850fac6abbf41db
thelifeisgood <besson.antho@hotmail.fr>
2016-04-12 13:58:17 +0200

    add category to product and implement search by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
 class ProductsController < ApplicationController
 	before_action :find_product, only: [:show, :edit, :update, :destroy]
 	def index
-		@products = Product.all
+		if params[:category]
+			@products = Product.where(category: params[:category])
+		else
+			@products = Product.all
+		end
 	end
 
 	def show
@@ -39,7 +43,7 @@ class ProductsController < ApplicationController
 	private
 
 	def product_params
-		params.require(:product).permit(:name, :url, :tagline)
+		params.require(:product).permit(:name, :url, :tagline, :category)
 	end
 
 	def find_product
