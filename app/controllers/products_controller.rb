b71550629810280e8d97be5d8be778524a41ac2c
thelifeisgood <besson.antho@hotmail.fr>
2016-04-12 09:45:44 +0200

    implement new and create actions on product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,23 +3,30 @@ class ProductsController < ApplicationController
 	def index
 		@products = Product.all
 	end
-
 	def show
 		@product = Product.find(params[:id])
 	end
-
 	def new		
+		@product = Product.new
 	end
-
-	def create		
+	def create	
+		@product = Product.new(product_params)
+		if @product.save
+			redirect_to products_path
+		else
+			render :new
+		end
 	end
-
 	def edit		
 	end
-
 	def update		
 	end
-
 	def destroy		
+	end
+
+	private
+
+	def product_params
+		params.require(:product).permit(:name, :url)
 	end
 end
